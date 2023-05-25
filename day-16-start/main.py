3ce1a729379fdcd7fda1c2d68ac3a218c838103a
Joshua <jhorton12@gmail.com>
2023-05-25 14:32:00 -0500

    test1

---
+++ day-16-start/main.py
@@ -0,0 +1,22 @@
+# import another_module
+# print(another_module.another_variable)
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkRed")
+# timmy.forward(100)
+
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Charizard", "Mew"])
+table.add_column("Type", ["Electric", "Fire-Flying", "Psychic"])
+
+table.align = "l"
+
+print(table)
